f7cb75957ef07d3de801faa3d883e806ccaa9ce9
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-20 08:37:57 +0200

    Linear Search Array

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,95 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DataStructuresAndAlgorithms.BigData;
+using DataStructuresAndAlgorithms.LinearSearch;
 
-Big0 big = new Big0();
-big.Description();
+int[] array = new int[] {1,2,3,4,5,6,7,8,9,10 };
 
+LinearSearchArray linearSearchArray = new LinearSearchArray();
 
-Console.WriteLine("===========");
-
-//Array Insetion & Deletions 
-
-int[] intArray = new int[10];
-
-int length = 0;
-
-
-for (int i = 0; i < 8; i++)
-{
-    intArray[length] = i+1;
-    length++;
-}
-
-foreach (var item in intArray)
-{
-    Console.WriteLine(item);
-}
-Console.WriteLine("----");
-//intArray[length] = 8;
-//length++;
-
-//Inserting at the start of an array
-
-for (int i = 3; i >= 0; i--)
-{
-    intArray[i + 1] = intArray[i];
-}
-
-intArray[0] = 20;
-int value = 0;
-
-foreach (var item in intArray)
-{
-    Console.WriteLine(item);
-}
-Console.WriteLine("---");
-
-for (int i = 4; i >= 2; i--)
-{
-    intArray[i + 1] = intArray[i];
-}
-
-intArray[2] = 8;
-foreach (var item in intArray)
-{
-    Console.WriteLine(item);
-}
-
-//Deletions
-
-Console.WriteLine("Deletions");
-int[] intArray2 = new int[9];
-
-int lengthh = 0;
-for (int i = 0; i < 6; i++)
-{
-    intArray2[lengthh] = i;
-    lengthh++;
-}
-//lengthh--;
-
-for (int i = 1; i < lengthh; i++)
-{
-    intArray2[i - 1] = intArray2[i];
-    length++;
-
-}
-lengthh--;
-
-for (int i = 0; i < lengthh; i++)
-{
-    Console.WriteLine(intArray2[i]);
-}
-
-Console.WriteLine("----");
-Console.WriteLine("Deleting From Anywhere in Array");
-for (int i = 4; i < lengthh; i++)
-{
-    intArray2[i - 1] = intArray2[i];
-}
-lengthh--;
-for (int i = 0; i < lengthh; i++)
-{
-    Console.WriteLine(intArray2[i]);
-}
+Console.WriteLine(linearSearchArray.LinearSearch(array,10));
+Console.WriteLine("===");
+Console.WriteLine(linearSearchArray.LinearSearchWhile(array,10));
