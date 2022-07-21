f16becdbaac618130a5268dccf13a9e251dcaefb
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-21 10:28:33 +0200

    Linked List

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -2,11 +2,35 @@
 
 using DataStructuresAndAlgorithms.BigData;
 using DataStructuresAndAlgorithms.LinearSearch;
+using DataStructuresAndAlgorithms.LinkedList;
 
-int[] array = new int[] {1,2,3,4,5,6,7,8,9,10 };
+Node nodeA = new Node();
+nodeA.Data = 865;
+Node nodeB = new Node();
+nodeB.Data = 344;
+Node nodeC = new Node();
+nodeC.Data = 888;
+Node nodeD = new Node();
+nodeD.Data = 222;
 
-LinearSearchArray linearSearchArray = new LinearSearchArray();
+nodeA.Next = nodeB;
+nodeB.Next = nodeC;
+nodeC.Next = nodeD;
 
-Console.WriteLine(linearSearchArray.LinearSearch(array,10));
 Console.WriteLine("===");
-Console.WriteLine(linearSearchArray.LinearSearchWhile(array,10));
+
+
+LinkedList linkedList = new LinkedList();
+linkedList.InsertFirst(1);
+linkedList.InsertFirst(2);
+linkedList.InsertFirst(3);
+linkedList.InsertFirst(4);
+
+
+linkedList.DeleteFirst();
+linkedList.DeleteFirst();
+
+linkedList.InsertLast(546);
+linkedList.InsertLast(3434);
+
+linkedList.DisplayList();
