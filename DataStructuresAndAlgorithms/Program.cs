68493bf5018460d361adfa63350cb15815522a85
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-24 08:35:01 +0200

    Binary Search

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,23 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DataStructuresAndAlgorithms.BigData;
+using DataStructuresAndAlgorithms.BinarySearch;
 using DataStructuresAndAlgorithms.LinearSearch;
 using DataStructuresAndAlgorithms.LinkedList;
 using DataStructuresAndAlgorithms.Queues;
 using DataStructuresAndAlgorithms.Stack;
 using System.Collections.Generic;
 
-// Kolekcje - przetrzymują elementy kolekcji jako obiekty.
-// Queue First In First Out
 
 
-Queue queue = new Queue(10);
-queue.Enqueue(1);
-queue.Enqueue(2);
-queue.Enqueue(3);
-queue.Enqueue(4);
 
-queue.Dequeue();
-queue.Dequeue();
+int[] intAraay = { -22, -15, 2, 7, 20, 30, 54 };
 
-queue.Peek();
+BinarySearch binarySearch = new BinarySearch();
+Console.WriteLine(binarySearch.BinarySearchMethod(intAraay, 2));
